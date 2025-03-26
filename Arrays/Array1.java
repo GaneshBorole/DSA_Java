55fdffae08f5c969bd5789ea7bca4a9e6d28ae43
ganeshborole <ganeshborole73@gmail.com>
2025-03-26 11:58:14 +0530

    binary search is done

--- Arrays/Array1.java
+++ Arrays/Array1.java
@@ -17,7 +17,7 @@ public class Array1 {
     //  }
 
 
-    //Array as a Function Argument
+//Array as a Function Argument
 
     // public static void update(int marks[]){
     //     for(int i=0;i<marks.length;i++){
@@ -35,36 +35,103 @@ public class Array1 {
     //     System.out.println();
     // }
     
-
    //Linear Search
 
-   public static int LinearSearch(int numbers[], int key) {
-       for (int i = 0; i < numbers.length; i++) {
-           if (numbers[i] == key) {
-               return i; // Return the index of the key
-               }
-            }
+//    public static int LinearSearch(int numbers[], int key) {
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (numbers[i] == key) {
+//                return i; // Return the index of the key
+//                }
+//             }
+//         return -1;// Return -1 if the key is not found
+//    }
+//    public static void main(String[] args) {
+//     Scanner sc= new Scanner(System.in);
+//     System.out.println("Enter the size of the array:");
+//     int size = sc.nextInt();
+//     int numbers[] = new int[size];
+//     System.out.println("Enter the elements of the array:");
+//     for (int i = 0; i < size; i++) {
+//         numbers[i] = sc.nextInt();
+//     }
+//     System.out.println("enter the Key element");
+//     int key= sc.nextInt();
+//     int index=LinearSearch(numbers, key);
+//     if(index == -1){
+//         System.out.println(" key Not found in array");
+//     }else{
+//         System.out.println("key is at index "+index);
+//     }
+//    }
+ 
+
+//Largest Number in Array
+
+
+// public static int getLargest(int numbers[]){
+//     int largest=Integer.MIN_VALUE;//-infinity
+//     for (int i=0;i<numbers.length;i++){
+//         if(largest < numbers[i]){
+//             largest=numbers[i];
+//         }
+//     }
+//     return largest;
+// }
+// public static void main(String[] args) {
+//     Scanner sc= new Scanner(System.in);
+//     System.out.println("enter the size of array");
+//     int size=sc.nextInt();
+//     int numbers[]=new int [size];
+//     System.out.println("enter the elements");
+//     for(int i=0;i<numbers.length;i++){
+//         numbers[i]=sc.nextInt();
         
-        return -1;
-        // Return -1 if the key is not found
-   }
-   public static void main(String[] args) {
+//     }
+//     System.out.println("largest value is "+getLargest(numbers));
+// }
+
+
+//Binary Search
+
+public static int BinarySearch(int numbers[], int key){
+    int start=0 ;
+    int end= numbers.length-1;
+
+    while (start<= end) {
+        int mid=(start+ end)/2;
+
+        //comaparisons
+        if(numbers[mid] == key){
+            return mid;
+        }
+        if(numbers[mid] < key){ //for 2nd half search
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
+    }
+    return -1; // Return -1 if the key is not found
+}
+public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
-    System.out.println("Enter the size of the array:");
-    int size = sc.nextInt();
-    int numbers[] = new int[size];
-    System.out.println("Enter the elements of the array:");
-    for (int i = 0; i < size; i++) {
-        numbers[i] = sc.nextInt();
+    System.out.println("enter the size of array");
+    int size= sc.nextInt();
+    int numbers[]=new int[size];
+    System.out.println("enter element in array");
+    for(int i=0;i<numbers.length;i++){
+        numbers[i]=sc.nextInt();
     }
-    System.out.println("enter the Key element");
+    System.out.println("enter the key");
     int key= sc.nextInt();
-    int index=LinearSearch(numbers, key);
-    if(index == -1){
-        System.out.println(" key Not found in array");
+
+    int index = BinarySearch(numbers, key);
+    if (index == -1) {
+        System.out.println("key doesn't exist in  found");
     }else{
-        System.out.println("key is at index "+index);
+        System.out.println("key at index "+index);
     }
-   }
-   }
+}
+
+
+}
  
