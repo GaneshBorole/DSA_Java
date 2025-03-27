618f62d602caa4cbee04582030c6affdb3188360
ganeshborole <ganeshborole73@gmail.com>
2025-03-27 12:00:46 +0530

    pairs of array done

--- Arrays/Array1.java
+++ Arrays/Array1.java
@@ -93,45 +93,123 @@ public class Array1 {
 
 //Binary Search
 
-public static int BinarySearch(int numbers[], int key){
-    int start=0 ;
-    int end= numbers.length-1;
+// public static int BinarySearch(int numbers[], int key){
+//     int start=0 ;
+//     int end= numbers.length-1;
 
-    while (start<= end) {
-        int mid=(start+ end)/2;
+//     while (start<= end) {
+//         int mid=(start+ end)/2;
 
-        //comaparisons
-        if(numbers[mid] == key){
-            return mid;
+//         //comaparisons
+//         if(numbers[mid] == key){
+//             return mid;
+//         }
+//         if(numbers[mid] < key){ //for 2nd half search
+//             start=mid+1;
+//         }else{
+//             end=mid-1;
+//         }
+//     }
+//     return -1; // Return -1 if the key is not found
+// }
+// public static void main(String[] args) {
+//     Scanner sc= new Scanner(System.in);
+//     System.out.println("enter the size of array");
+//     int size= sc.nextInt();
+//     int numbers[]=new int[size];
+//     System.out.println("enter element in array");
+//     for(int i=0;i<numbers.length;i++){
+//         numbers[i]=sc.nextInt();
+//     }
+//     System.out.println("enter the key");
+//     int key= sc.nextInt();
+
+//     int index = BinarySearch(numbers, key);
+//     if (index == -1) {
+//         System.out.println("key doesn't exist in  found");
+//     }else{
+//         System.out.println("key at index "+index);
+//     }
+// }
+
+
+    // public static void reverse(int numbers[]) {
+    //     int start = 0;
+    //     int end = numbers.length - 1;
+
+    //     while (start < end) {
+    //         int temp = numbers[end];
+    //         numbers[end] = numbers[start];
+    //         numbers[start] = temp;
+
+    //         start++;
+    //         end--;
+    //     }
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.println("Enter the size of array:");
+    //     int size = sc.nextInt();
+    //     int numbers[] = new int[size];
+
+    //     System.out.println("Enter the elements:");
+    //     for (int i = 0; i < numbers.length; i++) {
+    //         numbers[i] = sc.nextInt();
+    //     }
+
+    //     // Call reverse function after input is completed
+    //     reverse(numbers);
+
+    //     System.out.println("Reversed array:");
+    //     for (int i=0;i<numbers.length;i++) {
+    //         System.out.print(numbers[i] + " ");
+    //     }
+
+      
+    // }
+
+
+
+    //Pairs in array
+
+    
+        public static void pairsinArray(int numbers[]) {
+            for (int i = 0; i < numbers.length; i++) {
+                int curr = numbers[i];
+    
+                for (int j = i + 1; j < numbers.length; j++) {
+                    System.out.println("(" + curr + " , " + numbers[j] + ")");
+                }
+                System.out.println();
+            }
         }
-        if(numbers[mid] < key){ //for 2nd half search
-            start=mid+1;
-        }else{
-            end=mid-1;
+    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+    
+            System.out.println("Enter the size:");
+            int size = sc.nextInt();
+            int numbers[] = new int[size];
+    
+            System.out.println("Enter the elements:");
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = sc.nextInt();
+            }
+    
+            // Printing pairs
+            pairsinArray(numbers);
+    
+            // Corrected loop to print array elements
+            System.out.println("Array elements:");
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println(numbers[i] + " ");
+            }
+    
+            sc.close(); // Close Scanner to avoid memory leaks
         }
     }
-    return -1; // Return -1 if the key is not found
-}
-public static void main(String[] args) {
-    Scanner sc= new Scanner(System.in);
-    System.out.println("enter the size of array");
-    int size= sc.nextInt();
-    int numbers[]=new int[size];
-    System.out.println("enter element in array");
-    for(int i=0;i<numbers.length;i++){
-        numbers[i]=sc.nextInt();
-    }
-    System.out.println("enter the key");
-    int key= sc.nextInt();
+    
 
-    int index = BinarySearch(numbers, key);
-    if (index == -1) {
-        System.out.println("key doesn't exist in  found");
-    }else{
-        System.out.println("key at index "+index);
-    }
-}
-
-
-}
  
