995bed68bd7b3352d9107be37b7bedc3aba26b4b
ganeshborole <ganeshborole73@gmail.com>
2025-04-04 15:28:04 +0530

    Counting sort is done

--- Arrays/BasicSorting.java
+++ Arrays/BasicSorting.java
@@ -62,27 +62,61 @@ public class BasicSorting {
 
 
     //Insertion sort
-    public static void  InsertionSort(int arr[]){
+    // public static void  InsertionSort(int arr[]){
+    //     for(int i=0;i<arr.length;i++){
+    //         int curr= arr[i];
+    //         int prev=i-1;
+    //         while(prev >=0 && arr[prev]> curr){
+    //             arr[prev+1]=arr[prev];
+    //             prev--;
+    //         }
+    //         //insertion
+    //         arr[prev+1]=curr;
+    //     }
+
+    // }
+    // public static void PrintArr(int arr[]){
+    //     System.out.println("Sorted array is: ");
+    //      for(int i=0;i<arr.length;i++){
+    //         System.out.print(arr[i]+" ");
+
+    //      }
+    //      System.out.println();
+    // }
+
+
+
+    //Counting Sort
+
+    public static void CountingSort(int arr[]){
+        int largest=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
-            int curr= arr[i];
-            int prev=i-1;
-            while(prev >=0 && arr[prev]> curr){
-                arr[prev+1]=arr[prev];
-                prev--;
-            }
-            //insertion
-            arr[prev+1]=curr;
+            largest=Math.max(largest, arr[i]);
+        }
+        int count[]=new int[largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        //sorting
+        int j=0;
+        for(int i=0; i< count.length;i++){
+            while(count[i]>0){
+            arr[j]=i;
+            j++;
+            count[i]--;
+        }
+        }
+    }
+    
+    public static void PrintArr(int arr[]){
+            System.out.println("Sorted array is: ");
+             for(int i=0;i<arr.length;i++){
+                System.out.print(arr[i]+" ");
+    
+             }
+             System.out.println();
         }
 
-    }
-    public static void PrintArr(int arr[]){
-        System.out.println("Sorted array is: ");
-         for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-
-         }
-         System.out.println();
-    }
 public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
     System.out.println("enter the size of array");
@@ -94,11 +128,15 @@ public static void main(String[] args) {
     }
     // bubbleSort(arr);
     // selectionSort(arr);
-    InsertionSort(arr);
+    //InsertionSort(arr);
+    //Arrays.sort(arr); //inbuilt  function for sorting an elements
+    CountingSort(arr);
     PrintArr(arr);
        
     
 }
+
+
 
 
 }
