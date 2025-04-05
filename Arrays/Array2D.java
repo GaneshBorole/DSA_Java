3412bedbf3cd89eb77e46cb77b56b57644516eb4
ganeshborole <ganeshborole73@gmail.com>
2025-04-05 14:05:13 +0530

    swapping is done in arraylist

--- Arrays/Array2D.java
+++ Arrays/Array2D.java
@@ -69,22 +69,48 @@ public class Array2D {
 
 
     //Print Maximum nuber in ArrayList
+    // public static void main(String[] args) {
+    //     int max = Integer.MIN_VALUE;
+    //     List <Integer> list = new ArrayList<>();
+
+    //     list.add(2);
+    //     list.add(4);
+    //     list.add(6);
+    //     list.add(1);
+
+    //     for (int i = 0; i < list.size(); i++) {
+    //         if (max < list.get(i)) {
+    //             max = list.get(i);
+    //         }
+    //     }
+
+    //     System.out.println("Max element is: " + max);
+    // }
+
+
+
+    //swapping in between List elements
+
+    public static void swap(ArrayList <Integer> list, int indx1,int indx2){
+        int temp=list.get(indx1);
+        list.set(indx1,list.get(indx2));
+        list.set(indx2, temp);
+
+
+    }
     public static void main(String[] args) {
-        int max = Integer.MIN_VALUE;
-        List <Integer> list = new ArrayList<>();
-
-        list.add(2);
-        list.add(4);
-        list.add(6);
+        ArrayList<Integer> list= new ArrayList<>();
         list.add(1);
+        list.add(7);
+        list.add(4);
+        list.add(8);
+        list.add(2);
 
-        for (int i = 0; i < list.size(); i++) {
-            if (max < list.get(i)) {
-                max = list.get(i);
-            }
-        }
+        int indx1=1, indx2=2;
+        System.out.println(list);
+        swap(list, indx1, indx2);
+        System.out.println(list);
 
-        System.out.println("Max element is: " + max);
     }
     }
    
