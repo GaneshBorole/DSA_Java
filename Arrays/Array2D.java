55c117589003743007d4a4e1aea113550d768289
ganeshborole <ganeshborole73@gmail.com>
2025-04-08 17:35:44 +0530

    sum of diadonal in 2D array

--- Arrays/Array2D.java
+++ Arrays/Array2D.java
@@ -49,43 +49,86 @@ public class Array2D {
     // sc.close(); // Always a good practice
     // }
 
+
+
+
+
     // Spriral Matrix print
 
-    public static void PrintSprial(int matrix[][]) {
-        int startRow = 0;
-        int startCol = 0;
-        int endRow = matrix.length - 1;
-        int endcol = matrix[0].length - 1;
+    // public static void PrintSprial(int matrix[][]) {
+    //     int startRow = 0;
+    //     int startCol = 0;
+    //     int endRow = matrix.length - 1;
+    //     int endcol = matrix[0].length - 1;
 
-        while (startRow <= endRow && startCol <= endcol) {
-            // top boundary print
-            // i=row
-            // j= column
-            for (int j = 0; j <= endcol; j++) {
-                System.out.print(matrix[startRow][j] + " ");
-            }
-            // right boundary
-            for (int i = startRow + 1; i <= endRow; i++) {
-                System.out.print(matrix[i][endcol] + " ");
+    //     while (startRow <= endRow && startCol <= endcol) {
+    //         // top boundary print
+    //         // i=row
+    //         // j= column
+    //         for (int j = 0; j <= endcol; j++) {
+    //             System.out.print(matrix[startRow][j] + " ");
+    //         }
+    //         // right boundary
+    //         for (int i = startRow + 1; i <= endRow; i++) {
+    //             System.out.print(matrix[i][endcol] + " ");
 
-            }
-            // bootom boundary
-            for (int j = endcol - 1; j >= startCol; j--) {
-                System.out.print(matrix[endRow][j] + " ");
+    //         }
+    //         // bootom boundary
+    //         for (int j = endcol - 1; j >= startCol; j--) {
+    //             if(startRow==endRow){
+    //                 break;
+    //             }
+    //             System.out.print(matrix[endRow][j] + " ");
 
-            }
-            //left boundary
-            for (int i = endRow - 1; i >= startRow + 1; i--) {
-                System.out.print(matrix[i][startCol] + " ");
-            }
-            startCol++;
-            startRow++;
-            endcol--;
-            endRow--;
+    //         }
+    //         //left boundary
+    //         for (int i = endRow - 1; i >= startRow + 1; i--) {
+    //             if(startCol== endcol){
+    //                 break;
+    //             }
+    //             System.out.print(matrix[i][startCol] + " ");
+    //         }
+    //         startCol++;
+    //         startRow++;
+    //         endcol--;
+    //         endRow--;
 
+    //     }
+    //     System.out.println();
+
+    // }
+
+
+
+
+    // Diagonal Sum of elements
+
+    public static int  DiagonalSum(int matrix[][]){
+        int sum = 0;
+
+        // for(int i=0; i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(i== j){
+        //             sum += matrix[i][j];
+        //         }
+        //         else if(i+j == matrix.length-1){
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+        // return sum;
+
+
+        //method 2
+        for(int i=0;i<matrix.length;i++){
+            sum+=matrix[i][i]; //primary diagonal
+        
+        if (i != matrix.length- 1 - i) {
+            sum += matrix[i][matrix.length- 1 - i]; // secondary diagonal
         }
-        System.out.println();
-
+    }
+        return sum;
+    
     }
 
     public static void main(String[] args) {
@@ -109,8 +152,12 @@ public class Array2D {
             }
             System.out.println();
         }
-        System.out.println("Sprial Matrix is : ");
-        PrintSprial(matrix);
+        // System.out.println("Sprial Matrix is : ");
+        // PrintSprial(matrix);
+        System.out.println("Diagonals sum of given matrix is :");
+        System.out.println(DiagonalSum(matrix));
 
     }
+
+   
 }
