c65800e07e2009d8210f0d5249464387f5d9d800
ganeshborole <ganeshborole73@gmail.com>
2025-04-09 17:20:06 +0530

    Shortest path problem is done

--- Arrays/Strings.java
+++ Arrays/Strings.java
@@ -13,18 +13,43 @@ public class Strings {
     // }
 
 
-    public static boolean isPalindrome(String str){
-        for(int i=0;i<str.length()/2;i++){
-            int n= str.length();
-            if(str.charAt(i) != str.charAt(n-1-i)){ //not a palindrome
-                return false;
+    // public static boolean isPalindrome(String str){
+    //     for(int i=0;i<str.length()/2;i++){
+    //         int n= str.length();
+    //         if(str.charAt(i) != str.charAt(n-1-i)){ //not a palindrome
+    //             return false;
 
 
-            }
+    //         }
 
-        }
+    //     }
         
-        return true;
+    //     return true;
+    // }
+
+
+    //getShortest path
+
+    public static float getShortestPath(String str){
+        int x=0,y=0;
+
+        for(int i=0;i<str.length();i++){
+            char dir=str.charAt(i);
+
+            if(dir == 'S'){ //South
+                y--;
+            }else if(dir == 'N'){ //North
+                y++;
+            }else if(dir == 'W'){ //West
+                x--;
+            }else{ //East
+                x++;
+            }
+        }
+        int X2=x*x;
+        int Y2=y*y;
+        return(float) Math.sqrt(X2+Y2);
+
     }
     public static void main(String[] args) {
         // Scanner sc= new Scanner(System.in);
@@ -47,7 +72,9 @@ public class Strings {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter your String");
         String str=sc.nextLine();
-        System.out.println(isPalindrome(str));
+        // System.out.println(isPalindrome(str));
+        System.out.println("The Shortest Path is :");
+        System.out.println(getShortestPath(str));
 
     }
 
