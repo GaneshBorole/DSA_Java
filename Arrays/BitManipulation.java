9a421e1db0dcc38e4f709805e9c6a520786ad0c8
ganeshborole <ganeshborole73@gmail.com>
2025-04-12 11:11:53 +0530

    ith bit problem

--- Arrays/BitManipulation.java
+++ Arrays/BitManipulation.java
@@ -2,14 +2,23 @@ package Arrays;
 import java.util.*;
 
 public class BitManipulation {
-    public static void OddorEven(int n){
-        int bitmask=1;
-        if(( n & bitmask) == 0){
-            System.out.println("given number is even ");
-        }else{
-            System.out.println("given number is odd ");
-        }
+    // public static void OddorEven(int n){
+    //     int bitmask=1;
+    //     if(( n & bitmask) == 0){
+    //         System.out.println("given number is even ");
+    //     }else{
+    //         System.out.println("given number is odd ");
+    //     }
 
+    // }
+
+    public static int getIthbit(int n,int i){
+        int bitmask= 1<< i;
+        if((n & bitmask)==0){
+            return 0;
+        }else{
+            return 1;
+        }
     }
     public static void main(String[] args) {
         // System.out.println(5 & 6);
@@ -19,7 +28,11 @@ public class BitManipulation {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter your number");
         int n= sc.nextInt();
-        OddorEven(n);
+        System.out.println("enter the ith Bit");
+        int i= sc.nextInt();
+        // OddorEven(n);
+        System.out.println("ith bit is :");
+        System.out.println(getIthbit(n, i));
     }
     
 }
