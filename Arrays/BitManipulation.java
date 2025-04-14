74aadf7ec702171a114732be677a768a9bf2a459
ganeshborole <ganeshborole73@gmail.com>
2025-04-14 10:48:03 +0530

    set and get ithbit is done

--- Arrays/BitManipulation.java
+++ Arrays/BitManipulation.java
@@ -12,14 +12,30 @@ public class BitManipulation {
 
     // }
 
-    public static int getIthbit(int n,int i){
-        int bitmask= 1<< i;
-        if((n & bitmask)==0){
-            return 0;
-        }else{
-            return 1;
+
+        //How to get ith Bit 
+
+    // public static int getIthbit(int n,int i){
+    //     int bitmask= 1<< i;
+    //     if((n & bitmask)==0){
+    //         return 0;
+    //     }else{
+    //         return 1;
+    //     }
+    // }
+    
+
+        //Set ith Bit
+        public static int setIthbit(int n,int i){
+            int bitmask= 1<< i;
+            return n | bitmask;
         }
-    }
+
+        //clear ith Bit
+        public static int clearIthbit(int n, int i){
+            int bitmask= ~1<<i;
+            return n & bitmask;
+        }
     public static void main(String[] args) {
         // System.out.println(5 & 6);
         // System.out.println(5 ^ 6);
@@ -32,7 +48,9 @@ public class BitManipulation {
         int i= sc.nextInt();
         // OddorEven(n);
         System.out.println("ith bit is :");
-        System.out.println(getIthbit(n, i));
+        // System.out.println(getIthbit(n, i));
+        // System.out.println(setIthbit(n, i));
+        System.out.println(clearIthbit(n, i));
     }
     
 }
