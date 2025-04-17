bd3bbbd6c15c7bae8997b9c18b8b59da9c6ad1bb
ganeshborole <ganeshborole73@gmail.com>
2025-04-17 15:51:57 +0530

    recursion is done

---
+++ Arrays/Oops.java
@@ -0,0 +1,20 @@
+package Arrays;
+import java.util.*;
+
+
+public class Oops {
+    public static void main(String[] args) {
+       Student s1=new Student();
+
+    
+    }
+
+}
+
+class Student{
+    String name;
+    int roll;
+    Student(){
+        System.out.println("constructor is called...");
+    }
+}
--- Arrays/tempCodeRunnerFile.java
+++ Arrays/tempCodeRunnerFile.java
@@ -1,2 +1,1 @@
 
-        // System.out.println(5 & 6);
