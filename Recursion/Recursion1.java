a03179f12e715ab9a7375f9cbde171c82b5d89e5
ganeshborole <ganeshborole73@gmail.com>
2025-04-18 18:30:38 +0530

    print number in the increasing order

--- Recursion/Recursion1.java
+++ Recursion/Recursion1.java
@@ -10,9 +10,19 @@ public class Recursion1 {
         System.out.print(n+" ");
         printDesc(n-1);
     }
+
+    public static void printInc(int n){
+        if(n == 1){
+            System.out.print(1);
+            return;
+        }
+        printInc(n-1);
+        System.out.println(n+" ");
+    }
     public static void main(String[] args) {
-        int n=10;
-        printDesc(n);
+        int n=5;
+        // printDesc(n);
+        printInc(n);
     }
     
 }
