ff345195adb4d6a794ee5699a685f3568446cca6
ganeshborole <ganeshborole73@gmail.com>
2025-04-19 10:10:48 +0530

    fact and sum of numbers is calculated

--- Recursion/Recursion1.java
+++ Recursion/Recursion1.java
@@ -12,17 +12,38 @@ public class Recursion1 {
     }
 
     public static void printInc(int n){
-        if(n == 1){
+        if(n == 0){
             System.out.print(1);
             return;
         }
         printInc(n-1);
         System.out.println(n+" ");
     }
+
+    public static int Fact(int n){
+        if(n==0){
+            return 1;
+        }
+        int fnm1=Fact(n-1);
+        int fn=n* Fact(n-1);
+        return fn;
+    }
+    public static int sum (int n){
+        if(n == 1){
+            return 1;
+        }
+        int abc=sum(n-1);
+        int sm=n+sum(n-1);
+        return sm;
+    }
     public static void main(String[] args) {
-        int n=5;
+      Scanner sc= new Scanner(System.in);
+      System.out.println("enter n");
+      int n=sc.nextInt();
         // printDesc(n);
-        printInc(n);
+        // printInc(n);
+        // System.out.println(Fact(n));
+        System.out.println(sum(n));
     }
     
 }
