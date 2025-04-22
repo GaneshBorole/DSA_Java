13c5880d590da169d80e25756f739742ff6fd021
ganeshborole <ganeshborole73@gmail.com>
2025-04-22 16:25:58 +0530

    remove duplicate character  in string

--- Recursion/Recursion2.java
+++ Recursion/Recursion2.java
@@ -14,11 +14,28 @@ public class Recursion2 {
         int totalways=fnm1+fnm2;
         return totalways;
     }
+    //remove Duplicates in String
+    public static void removeduplicates(String str,int idx,StringBuilder newStr,boolean map[]){
+        if(idx==str.length()){
+            System.out.println(newStr);
+            return;
+
+        }
+        char currChar=str.charAt(idx);
+        if(map[currChar-'a']==true){
+            removeduplicates(str, idx+1, newStr, map);
+        }else{
+            map[currChar-'a']=true;
+            removeduplicates(str, idx+1, newStr.append(currChar), map);
+        }
+    }
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("enter the n");
-        int n= sc.nextInt();
-        System.out.println("Total ways to tile 2 x " + n + " board: " + TailingProblem(n));
+        // Scanner sc= new Scanner(System.in);
+        // System.out.println("enter the n");
+        // int n= sc.nextInt();
+        // System.out.println("Total ways to tile 2 x " + n + " board: " + TailingProblem(n));
+        String str="apnnnacollege";
+      removeduplicates(str, 0, new StringBuilder(" "), new boolean[26]);
        
     }
     
