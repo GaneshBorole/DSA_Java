fd7999539e75fcff425b3cd13e36c293f1704e4c
ganeshborole <ganeshborole73@gmail.com>
2025-05-03 15:09:58 +0530

    max element in arraylist

--- ArrayList/Arraylist.java
+++ ArrayList/Arraylist.java
@@ -6,33 +6,43 @@ public class Arraylist {
     public static void main(String[] args) {
         ArrayList<Integer>list=new ArrayList<>();
         //add operation
-        list.add(1);
+        list.add(8);
         list.add(2);
-        list.add(3);
+        list.add(9);
         list.add(4);
-        System.out.println(list);
-       System.out.println(list.size());
-        
-        Get operation
-        int element=list.get(0);
-       System.out.println(element);
-
-        Delete operation
-       int deleteEle=list.remove(2);
-        System.out.println(deleteEle);
-
-        set element at index
-        list.set(2, 7);
-        System.out.println(list);
-
-        contains
-        System.out.println(list.contains(9));
-
-        //print reverse numbers
-        for(int i= list.size()-1;i>=0;i--){
-            System.out.print(list.get(i)+" ");
+       
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<list.size();i++){
+            if(max<list.get(i)){
+                max=list.get(i);
+            }
         }
-        System.out.println();
+        System.out.println(max);
+
+
+    //    System.out.println(list);
+    //    System.out.println(list.size());
+        
+    //     Get operation
+    //     int element=list.get(0);
+    //    System.out.println(element);
+
+    //     Delete operation
+    //    int deleteEle=list.remove(2);
+    //     System.out.println(deleteEle);
+
+    //     set element at index
+    //     list.set(2, 7);
+    //     System.out.println(list);
+
+    //     contains
+    //     System.out.println(list.contains(9));
+
+    //     //print reverse numbers
+    //     for(int i= list.size()-1;i>=0;i--){
+    //         System.out.print(list.get(i)+" ");
+    //     }
+    //     System.out.println();
     
     }
     
