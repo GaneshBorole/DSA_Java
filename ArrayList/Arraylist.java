6878ddb73445d0a6d7dfa1b31f4d5dd36ea1cc89
ganeshborole <ganeshborole73@gmail.com>
2025-05-04 20:35:49 +0530

    multiDarraylist is created

--- ArrayList/Arraylist.java
+++ ArrayList/Arraylist.java
@@ -3,6 +3,11 @@ import java.util.*;
 
 
 public class Arraylist {
+    public static void swap(ArrayList<Integer>list,int idx1,int idx2){
+        int temp=list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
     public static void main(String[] args) {
         ArrayList<Integer>list=new ArrayList<>();
         //add operation
@@ -10,14 +15,22 @@ public class Arraylist {
         list.add(2);
         list.add(9);
         list.add(4);
+
+        // int idx1=1 ,idx2=3;
+        // System.out.println(list);
+        // swap(list, idx1, idx2);
+        // System.out.println(list);
+        System.out.println(list);;
+        Collections.sort(list);
+        System.out.println(list);
        
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<list.size();i++){
-            if(max<list.get(i)){
-                max=list.get(i);
-            }
-        }
-        System.out.println(max);
+        // int max=Integer.MIN_VALUE;
+        // for(int i=0;i<list.size();i++){
+        //     if(max<list.get(i)){
+        //         max=list.get(i);
+        //     }
+        // }
+        // System.out.println(max);
 
 
     //    System.out.println(list);
