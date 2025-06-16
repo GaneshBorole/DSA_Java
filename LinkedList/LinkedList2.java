e0b92065b65592a96bd8653f23f4b3a4f185ed03
ganeshborole <ganeshborole73@gmail.com>
2025-06-16 22:12:40 +0530

    remove circular LinkedList

--- LinkedList/LinkedList2.java
+++ LinkedList/LinkedList2.java
@@ -2,9 +2,9 @@ package LinkedList;
 
 public class LinkedList2 {
 
-    static Node head; // Declare head here
+    public static Node head; // Declare head here
 
-    static class Node {
+    public static class Node {
         int data;
         Node next;
 
@@ -13,6 +13,7 @@ public class LinkedList2 {
         }
     }
 
+    //  1 Detect cycle in linked list using Floyd's Cycle Detection Algorithm
     public static boolean isCycle(){
         Node slow = head;
         Node fast = head;
@@ -26,12 +27,48 @@ public class LinkedList2 {
         }
         return false;
     }
+
+
+
+// 2 remove cycle in linked list
+public static void removecycle(){
+    Node slow=head;
+    Node fast=head;
+    boolean exists=false;
+    while (fast !=null && fast.next!= null) {
+        slow= slow.next;
+        fast=fast.next.next;
+        if (slow == fast) {
+          exists=true;
+          break;         
+        }   
+    }
+    if(exists==false){
+        return;
+    }
+    //find meeting points
+    slow =head;
+    Node prev= null;
+    while (slow != fast) {
+        prev=fast;
+        slow=slow.next;
+        fast=fast.next;
+        
+    }
+    //remove  cycle ->last.next=null
+    prev.next=null;
+
+}
+
   
     public static void main(String[] args) {
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next=temp;
         head.next.next = new Node(3);
-        head.next.next.next = head; // creating a loop
-        System.out.println(isCycle()); // Should return true
+        head.next.next.next = temp; // creating a loop
+        System.out.println(isCycle()); 
+        removecycle();
+        System.out.println(isCycle());
     }
 }
