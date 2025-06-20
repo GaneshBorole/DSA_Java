8c112f356b578c58983d55b920c1727afbb7b9d1
ganeshborole <ganeshborole73@gmail.com>
2025-06-20 11:42:39 +0530

    push at bottom of stack

--- Stack/Stack1.java
+++ Stack/Stack1.java
@@ -1,6 +1,8 @@
 package Stack;
 
 import java.util.ArrayList;
+import java.util.*;
+
 
 public class Stack1 {
     // 1.stsck using arraylist
@@ -37,65 +39,90 @@ public class Stack1 {
 
     //2.stack using LL
 
-    
-    // Node class for the stack
-    static class Node {
-        int data;
-        Node next;
+    // // Node class for the stack
+    // static class Node {
+    //     int data;
+    //     Node next;
 
-        Node(int data) {
-            this.data = data;
-            this.next = null;
+    //     Node(int data) {
+    //         this.data = data;
+    //         this.next = null;
+    //     }
+    // }
+
+    // // Stack implementation using Linked List
+    // static class Stack {
+    //     Node head = null;
+
+    //     public boolean isEmpty() {
+    //         return head == null;
+    //     }
+
+    //     // push method
+    //     public void push(int data) {
+    //         Node newNode = new Node(data);
+    //         newNode.next = head;
+    //         head = newNode;
+    //     }
+
+    //     // pop method
+    //     public int pop() {
+    //         if (isEmpty()) {
+    //             System.out.println("Stack is empty");
+    //             return -1;
+    //         }
+    //         int top = head.data;
+    //         head = head.next;
+    //         return top;
+    //     }
+
+    //     // peek method
+    //     public int peek() {
+    //         if (isEmpty()) {
+    //             System.out.println("Stack is empty");
+    //             return -1;
+    //         }
+    //         return head.data;
+    //     }
+    // }
+
+    // public static void main(String[] args) {
+    //     Stack s = new Stack();
+
+    //     s.push(1);
+    //     s.push(2);
+    //     s.push(3);
+    //     s.push(4);
+
+    //     while (!s.isEmpty()) {
+    //         System.out.println(s.peek());
+    //         s.pop();
+    //     }
+    // }
+
+
+
+
+    //3.Push bottom of stack
+    public static void pushAtBottom(Stack<Integer>s,int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
         }
+        int top=s.pop();
+         pushAtBottom(s, data);
+         s.push(top);
     }
-
-    // Stack implementation using Linked List
-    static class Stack {
-        Node head = null;
-
-        public boolean isEmpty() {
-            return head == null;
-        }
-
-        // push method
-        public void push(int data) {
-            Node newNode = new Node(data);
-            newNode.next = head;
-            head = newNode;
-        }
-
-        // pop method
-        public int pop() {
-            if (isEmpty()) {
-                System.out.println("Stack is empty");
-                return -1;
-            }
-            int top = head.data;
-            head = head.next;
-            return top;
-        }
-
-        // peek method
-        public int peek() {
-            if (isEmpty()) {
-                System.out.println("Stack is empty");
-                return -1;
-            }
-            return head.data;
-        }
-    }
-
     public static void main(String[] args) {
-        Stack s = new Stack();
-
+        Stack<Integer> s= new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-        s.push(4);
 
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
+        pushAtBottom(s,4);
+             while (!s.isEmpty()) {
+            System.out.println(s.pop());       
         }
     }
-}
+    }
+
