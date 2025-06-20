5867914a1cc6cdf5f59cca12a787ed41e9b7758d
ganeshborole <ganeshborole73@gmail.com>
2025-06-20 12:01:41 +0530

    reverse a String using stack

--- Stack/Stack1.java
+++ Stack/Stack1.java
@@ -113,16 +113,39 @@ public class Stack1 {
          pushAtBottom(s, data);
          s.push(top);
     }
-    public static void main(String[] args) {
-        Stack<Integer> s= new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
 
-        pushAtBottom(s,4);
-             while (!s.isEmpty()) {
-            System.out.println(s.pop());       
+
+    //3.Reverse a String in stack
+
+    public static String reverseString(String str){
+        Stack<Character> s= new Stack<>();
+        int idx=0;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+            
         }
+        StringBuilder result= new StringBuilder("");
+        while (!s.isEmpty()) {
+            char curr=s.pop();
+            result.append(curr);
+            
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        // Stack<Integer> s= new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+
+        // pushAtBottom(s,4);
+        //      while (!s.isEmpty()) {
+        //     System.out.println(s.pop());       
+        // }
+        String str="abc";
+        String result=reverseString(str);
+        System.out.println(result);
     }
     }
 
