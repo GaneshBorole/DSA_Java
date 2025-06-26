45e4d6a59f8b9960c5c13053af5ef5d793aad639
ganeshborole <ganeshborole73@gmail.com>
2025-06-26 20:53:13 +0530

    duplicate parentheses check problem is done

--- Stack/stack2.java
+++ Stack/stack2.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class stack2 {
     //1.valid Parentheses check
     public static boolean isValid(String str ){
-        Stack<Character>s=new Stack<>();
+        Stack<Character> s=new Stack<>();
 
         for(int i=0;i<str.length();i++){
             char ch=str.charAt(i);
@@ -30,11 +30,40 @@ public class stack2 {
         }else{
             return false;
         }
+    }
 
+    //
+    public static boolean isDuplicate(String str){
+        Stack<Character> s=new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+
+            //closing
+            if(ch==')') {
+                int count=0;
+                while (s.peek()!= '(') {
+                    s.pop();
+                    count++;
+                    
+                }
+                if (count < 1) {
+                    return true;//duplicate exist
+                    
+                }else{
+                    s.pop();
+                }
+            }else{
+                //opening
+                s.push(ch);
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
-        String str="({})[]";//true
-        System.out.println(isValid(str));
+        String str1="((a+b))";//true
+        String str2="(a-b)";//false
+        System.out.println(isDuplicate(str2));
     }
     
 }
