4a63ad19a67e7f39411921c712ad2341064777b2
ganeshborole <ganeshborole73@gmail.com>
2025-06-29 13:30:32 +0530

    queue using LL

--- Queue/Queue1.java
+++ Queue/Queue1.java
@@ -3,76 +3,124 @@ import java.util.*;
 
 
 public class Queue1 {
-    static class Queue {
-        int[] arr;
-        int size;
-        int rear;
-        int front;
+//     static class Queue {
+//         int[] arr;
+//         int size;
+//         int rear;
+//         int front;
 
-        Queue(int n) {
-            arr = new int[n];
-            size = n;
-            rear = -1;
-            front = -1;
-        }
+//         Queue(int n) {
+//             arr = new int[n];
+//             size = n;
+//             rear = -1;
+//             front = -1;
+//         }
 
-        public boolean isEmpty() {
-            return rear == -1 && front == -1;
-        }
-        public boolean isFull(){
-            return (rear+1) % size== front;
-        }
+//         public boolean isEmpty() {
+//             return rear == -1 && front == -1;
+//         }
+//         public boolean isFull(){
+//             return (rear+1) % size== front;
+//         }
 
-        // Add
-        public void add(int data) {
-            if (isFull()) {
-                System.out.println("Queue is full");
-                return;
-            }
-            if(front == -1){
-                front =0;
-            }
-            rear=(rear+1) % size ;
-            arr[rear] = data;
-        }
+//         // Add
+//         public void add(int data) {
+//             if (isFull()) {
+//                 System.out.println("Queue is full");
+//                 return;
+//             }
+//             if(front == -1){
+//                 front =0;
+//             }
+//             rear=(rear+1) % size ;
+//             arr[rear] = data;
+//         }
 
-        // Remove
-        public int remove() {
-    if (isEmpty()) {
-        System.out.println("Queue is empty");
-        return -1;
-    }
-    int result = arr[front];
+//         // Remove
+//         public int remove() {
+//     if (isEmpty()) {
+//         System.out.println("Queue is empty");
+//         return -1;
+//     }
+//     int result = arr[front];
     
-    // Only one element in queue
-    if (front == rear) {
-        front = rear = -1;
-    } else {
-        front = (front + 1) % size;
+//     // Only one element in queue
+//     if (front == rear) {
+//         front = rear = -1;
+//     } else {
+//         front = (front + 1) % size;
+//     }
+
+//     return result;
+// }
+//         // Peek
+//         public int peek() {
+//             if (isEmpty()) {
+//                 System.out.println("Queue is empty");
+//                 return -1;
+//             }
+//             return arr[front];
+//         }
+//     }
+
+    //Queue using Linked list
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
     }
+    static class Queue {
+        static Node head=null;
+        static Node tail=null;
 
-    return result;
-}
+    public static boolean isEmpty(){
+        return head==null && tail==null;
+    }
+    //add
+    public static void add(int data){
+        Node newNode =new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+    //remove
+    public static int remove(){
+        if(isEmpty()){
+            System.out.println("queue is empty");
+            return -1;
+        }
+        int front=head.data;
+        if(tail ==head){
+            tail=head=null;
 
-
-        // Peek
+        }else{
+            head=head.next;
+        }
+        return front;
+    }
+     // Peek
         public int peek() {
             if (isEmpty()) {
                 System.out.println("Queue is empty");
                 return -1;
             }
-            return arr[front];
+            return head.data;
         }
-    }
+     }
+        
+    
     public static void main(String[] args) {
-        Queue q= new Queue(3);
+        Queue q= new Queue();
         q.add(1);
         q.add(2);
         q.add(3);
-        System.out.println(q.remove());
-        q.add(4);
-        System.out.println(q.remove());
-        q.add(5);
 
         while (!q.isEmpty()) {
             System.out.println(q.peek());
