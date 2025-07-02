472f339f9fcf3acb023167c8fd037cbe6f6cbd65
ganeshborole <ganeshborole73@gmail.com>
2025-07-02 18:38:33 +0530

    Stack using Deque

--- Queue/Queue1.java
+++ Queue/Queue1.java
@@ -285,35 +285,73 @@ public static void interLeave(Queue<Integer> q){
 
 }
 
-//6.Queue Reverse
-public static void reverse(Queue<Integer> q){
-    Stack<Integer> s=new Stack<>();
+// //6.Queue Reverse
+// public static void reverse(Queue<Integer> q){
+//     Stack<Integer> s=new Stack<>();
 
-    while (!q.isEmpty()) {
-        s.push(q.remove());
+//     while (!q.isEmpty()) {
+//         s.push(q.remove());
         
-    }
-    while (!s.isEmpty()) {
-        q.add(s.pop());
+//     }
+//     while (!s.isEmpty()) {
+//         q.add(s.pop());
         
+//     }
+// }
+
+
+
+
+
+static class Stack {
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void push(int data) {
+            deque.addLast(data); // Add to the end
+        }
+
+        public int pop() {
+            return deque.removeLast(); // Remove from the end
+        }
+
+        public int peek() {
+            return deque.getLast(); // Peek from the end
+        }
+
+        public boolean isEmpty() {
+            return deque.isEmpty();
+        }
     }
-}
 
     public static void main(String[] args) {
-        Queue<Integer> q=new LinkedList<>();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        q.add(4);
-        q.add(5);
-        q.add(6);
-        q.add(7);
-        q.add(8);
-        q.add(9);
-        q.add(10);
+        Stack s = new Stack();  // ✅ Corrected here
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        System.out.println("peek: " + s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+    
+
+
+        // Queue<Integer> q=new LinkedList<>();
+        // Deque<Integer> deque=new LinkedList<>();
+        // deque.addFirst(1);
+        // deque.addFirst(2);
+        // System.out.println(deque);
+        
+        
+
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // q.add(5);
+        
 
         // interLeave(q);
-        reverse(q);
+        // reverse(q);
 
 
     //     String str="aabccxb";
@@ -324,10 +362,10 @@ public static void reverse(Queue<Integer> q){
         // s.push(2);
         // s.push(3);
 
-        while (!q.isEmpty()) {
-            System.out.println(q.remove()+" ");
+        // while (!q.isEmpty()) {
+        //     System.out.println(q.remove()+" ");
           
-        }
-        System.out.println();
+        // }
+        // System.out.println();
     }
 }
