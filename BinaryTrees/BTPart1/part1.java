c014e7d1fae932d1b5cf3724826629bc00788eb2
ganeshborole <ganeshborole73@gmail.com>
2025-07-08 10:41:00 +0530

    preorder traversal is done

--- BinaryTrees/BTPart1/part1.java
+++ BinaryTrees/BTPart1/part1.java
@@ -28,12 +28,22 @@ public class part1 {
             return newNode;
 
         }
+        //preorder traversal
+        public static void preorder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
    public static void main(String[] args) {
     int node[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
     BinaryTree tree = new BinaryTree();
     Node root = tree.buildtree(node);
-    System.out.println(root.data);
+    // System.out.println(root.data);
+    tree.preorder(root);
 }
 
     
