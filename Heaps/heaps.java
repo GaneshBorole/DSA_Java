36eab7155928a18802acf473a429a5c93c3a3b83
ganeshborole <ganeshborole73@gmail.com>
2025-07-19 09:31:13 +0530

    delete element in heap

--- Heaps/heaps.java
+++ Heaps/heaps.java
@@ -30,11 +30,52 @@ public class heaps {
                 int temp=arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par, temp);
+
+                x=par;
+                par=(x-1)/2;
                 
             }
         }
-         public void print() {
-            System.out.println("Heap array: " + arr);
+         public int peek() {
+          return arr.get(0);
+        }
+
+        // Delete Node in Heap using heapify
+        private void heapify(int i){
+            int left=2*i+1;
+            int right=2*i+2;
+            int minIdx=i;
+        if (left < arr.size() && arr.get(minIdx)> arr.get(left)) {
+            minIdx=left;  
+        }
+         if (right < arr.size() && arr.get(minIdx)> arr.get(right)) {
+            minIdx=right;   
+        }
+        if (minIdx !=i) {
+            int tenp=arr.get(i);
+            arr.set(i, arr.get(minIdx));
+            arr.set(minIdx, tenp);
+            heapify(minIdx);
+            
+        }
+
+        }
+        public int remove(){
+            int data=arr.get(0);
+            //swap 1st and last
+            int temp=arr.get(0);
+            arr.set(0, arr.get(arr.size()-1));
+            arr.set(arr.size()-1, temp);
+
+            //delete last
+            arr.remove(arr.size()-1);
+
+            //heapify
+            heapify(0);
+            return data;
+        }
+        public boolean isEmpty(){
+            return arr.size()==0;
         }
  
     }
@@ -54,12 +95,17 @@ public class heaps {
         //     pq.remove();  // O(log n)
         // }
 
-         Heap h = new Heap();
-        h.add(5);
+
+        Heap h= new Heap();
         h.add(3);
-        h.add(2);
         h.add(4);
         h.add(1);
-        h.print();
+        h.add(5);
+
+        while (!h.isEmpty()) {
+            System.out.println(h.peek());
+            h.remove();
+            
+        }
     }
 }
