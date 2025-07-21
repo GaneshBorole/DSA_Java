40c3a57e7d4cca8b09249d65949ec744cacbd15e
ganeshborole <ganeshborole73@gmail.com>
2025-07-21 18:19:15 +0530

    basic hashmap operation done

---
+++ Hashing/Hashing.java
@@ -0,0 +1,29 @@
+package Hashing;
+import java.util.*;
+
+public class Hashing {
+    public static void main(String[] args) {
+        HashMap<String,Integer>hm= new HashMap<>();
+        //inset O(1)
+        hm.put("India",125);
+        hm.put("china", 150);
+        hm.put("us", 50);
+        System.out.println(hm);
+       
+        //Get O(1)
+        int population=hm.get("India");
+        System.out.println(population);
+        System.out.println(hm.get("pak"));
+
+        //conatinsKey O(1)
+        System.out.println(hm.containsKey("India"));
+        System.out.println(hm.containsKey("australia"));
+
+        //remove O(1)
+        System.out.println(hm.remove("India"));
+        System.out.println(hm);
+
+        //size
+        System.out.println(hm.size());
+    }
+}
