713534f039d9473b2e72e5246f7cc277e32532b1
ganeshborole <ganeshborole73@gmail.com>
2025-07-25 12:17:14 +0530

    hashing done

--- Hashing/hashmapImplement.java
+++ Hashing/hashmapImplement.java
@@ -1,4 +1,5 @@
 package Hashing;
+
 import java.util.*;
 
 public class hashmapImplement {
@@ -18,7 +19,7 @@ public class hashmapImplement {
         private LinkedList<Node>[] buckets;
 
         @SuppressWarnings("unchecked")
-        public HashMap() { 
+        public HashMap() {
             this.N = 4;
             this.buckets = new LinkedList[N];
             for (int i = 0; i < N; i++) {
@@ -61,8 +62,8 @@ public class hashmapImplement {
 
         // put element code
         public void put(K key, V value) {
-            int bi = hashFunction(key); //bi =bucket index
-            int di = searchLL(key, bi); //di=data index
+            int bi = hashFunction(key); // bi =bucket index
+            int di = searchLL(key, bi); // di=data index
 
             if (di != -1) {
                 buckets[bi].get(di).value = value;
@@ -76,7 +77,7 @@ public class hashmapImplement {
                 rehash();
             }
         }
-    
+
         // remove element code
         public V remove(K key) {
             int bi = hashFunction(key);
@@ -95,7 +96,6 @@ public class hashmapImplement {
             int di = searchLL(key, bi);
             return di != -1;
         }
-
 
         // Get element code
         public V get(K key) {
@@ -123,9 +123,8 @@ public class hashmapImplement {
         }
     }
 
-
-     // 2.check if Two strings are Anagram or Not
-   public static boolean isAnagram(String s, String t) {
+    // 2.check if Two strings are Anagram or Not
+    public static boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
         }
@@ -159,21 +158,20 @@ public class hashmapImplement {
         return map.isEmpty();
     }
 
-    
-
-public static String getStart(HashMap<String,String> tickets){
-    HashMap<String,String> revmap= new HashMap<>();
-    for(String key: tickets.keySet()){
-        revmap.put(tickets.get(key), key);
-    }
-    for(String key: tickets.keySet()){
-        if (!revmap.containsKey(key)) {
-            return key;
+    public static String getStart(HashMap<String, String> tickets) {
+        HashMap<String, String> revmap = new HashMap<>();
+        for (String key : tickets.keySet()) {
+            revmap.put(tickets.get(key), key);
         }
-    }
-    return null;
+        for (String key : tickets.keySet()) {
+            if (!revmap.containsKey(key)) {
+                return key;
+            }
+        }
+        return null;
 
-}
+    }
+
     public static void main(String[] args) {
         // HashMap<String, Integer> hm = new HashMap<>();
         // hm.put("India", 125);
@@ -183,18 +181,16 @@ public static String getStart(HashMap<String,String> tickets){
 
         // ArrayList<String> keys = hm.keySet();
         // for (String key : keys) {
-        //     System.out.println(key + " -> " + hm.get(key));
+        // System.out.println(key + " -> " + hm.get(key));
         // }
 
-
-        // LinkedHashMap<String,Integer> lhm=new LinkedHashMap<>(); // keys are inserted in ordered
+        // LinkedHashMap<String,Integer> lhm=new LinkedHashMap<>(); // keys are inserted
+        // in ordered
         // lhm.put("india", 100);
         // lhm.put("china", 150);
         // lhm.put("Us", 50);
 
         // System.out.println(lhm);
-
-
 
         // TreeMap<String,Integer> tm = new TreeMap<>(); // keys are sorted in ordered
         // tm.put("us", 80);
@@ -202,95 +198,90 @@ public static String getStart(HashMap<String,String> tickets){
         // tm.put("india", 100);
         // System.out.println(tm);
 
-
-        String s="race";
-        String t= "care";
+        String s = "race";
+        String t = "care";
         System.out.println(isAnagram(s, t));
 
+        // Hashset concept
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(4);
+        set.add(2);
+        System.out.println(set);
+        System.out.println(set.size());
+        // set.remove(2);
+        if (set.contains(2)) {
+            System.out.println("set contains 2");
 
+        }
 
-    // Hashset concept
-    HashSet<Integer> set=new HashSet<>();
-    set.add(1);
-    set.add(2);
-    set.add(4);
-    set.add(2);
-    System.out.println(set);
-    System.out.println(set.size());
-    //set.remove(2);
-    if (set.contains(2)) {
-        System.out.println("set contains 2");
-        
-    }
+        // iteration on hashset
+        HashSet<String> cities = new HashSet<>();
+        cities.add("Mumbai");
+        cities.add("pune");
+        cities.add("Delhi");
+        Iterator itr = cities.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
 
-    //iteration on hashset
-    HashSet<String> cities=new HashSet<>();
-    cities.add("Mumbai");
-    cities.add("pune");
-    cities.add("Delhi");
-    Iterator itr=cities.iterator();
-    while (itr.hasNext()) {
-        System.out.println(itr.next());
-        
-    }
-    for (String city : cities) {
-        System.out.println(city);
-        
-    }
+        }
+        for (String city : cities) {
+            System.out.println(city);
 
+        }
 
-    //LinkedHashset
-    LinkedHashSet<String> lhm=new LinkedHashSet<>();
-    lhm.add("Mumbai");
-    lhm.add("pune");
-    lhm.add("Delhi");
-    System.out.println(lhm);
+        // LinkedHashset
+        LinkedHashSet<String> lhm = new LinkedHashSet<>();
+        lhm.add("Mumbai");
+        lhm.add("pune");
+        lhm.add("Delhi");
+        System.out.println(lhm);
 
-    //treeset
-    TreeSet<String> trs=new TreeSet<>();
-    trs.add("mumbai");
-    trs.add("pune");
-    trs.add("Bengluru");
-    System.out.println(trs);
-    
-    // 1 count Distinct element in array
-            int nums[]={4,3,2,5,6,7,3,4,2,1};
-            HashSet<Integer> sets=new HashSet<>();
-            for (int i = 0; i < nums.length; i++) {
-                sets.add(nums[i]);
-                
+        // treeset
+        TreeSet<String> trs = new TreeSet<>();
+        trs.add("mumbai");
+        trs.add("pune");
+        trs.add("Bengluru");
+        System.out.println(trs);
+
+        // 1 count Distinct element in array
+        int nums[] = { 4, 3, 2, 5, 6, 7, 3, 4, 2, 1 };
+        HashSet<Integer> sets = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            sets.add(nums[i]);
+
+        }
+        System.out.println("ans = " + sets.size());
+
+        // 2 Union & Intersection of 2 arrays
+        int arr1[] = { 7, 3, 9 };
+        int arr2[] = { 6, 3, 9, 2, 9, 4 };
+        HashSet<Integer> set1 = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+            set1.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            set1.add(arr2[i]);
+        }
+        System.out.println("union of array = " + set1.size());
+
+        set1.clear();
+        for (int i = 0; i < arr1.length; i++) {
+            set1.add(arr1[i]);
+        }
+        int count = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            if (set1.contains(arr2[i])) {
+                count++;
+                set1.remove(arr2[i]);
+
             }
-            System.out.println("ans = "+sets.size());
 
-    
-    //2 Union & Intersection of 2 arrays
-                int arr1[]={7,3,9};
-                int arr2[]={6,3,9,2,9,4};
-                HashSet<Integer> set1= new HashSet<>();
-                for (int i = 0; i < arr1.length; i++) {
-                    set1.add(arr1[i]); 
-                }
-                for (int i = 0; i < arr2.length; i++) {
-                    set1.add(arr2[i]);
-                }
-                System.out.println("union of array = "+set1.size());
+        }
+        System.out.println("intersection of array =" + count);
 
-                set1.clear();
-                 for (int i = 0; i < arr1.length; i++) {
-                    set1.add(arr1[i]); 
-                }
-                int count=0;
-                for (int i = 0; i< arr2.length; i++) {
-                    if (set1.contains(arr2[i])) {
-                        count++;
-                        set1.remove(arr2[i]);
-                        
-                    }
-                    
-                }
-                System.out.println("intersection of array ="+count);
-
-        //3.find  Itinerary from ticket
+        // 3.find Itinerary from ticket
 
         HashMap<String, String> tickets = new HashMap<>();
         tickets.put("chennai", "Bengaluru");
@@ -309,29 +300,27 @@ public static String getStart(HashMap<String,String> tickets){
 
         System.out.println();
 
-
-        //1. find elements that appear more than(n/3) times
-        int arr[]={1,3,2,5,1,3,1,5,1};
-        HashMap<Integer,Integer> map = new HashMap<>();
+        // 1. find elements that appear more than(n/3) times
+        int arr[] = { 1, 3, 2, 5, 1, 3, 1, 5, 1 };
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             if (map.containsKey(arr[i])) {
-               map.put(arr[i], map.get(arr[i]) + 1); 
-                
-            }else{
+                map.put(arr[i], map.get(arr[i]) + 1);
+
+            } else {
                 map.put(arr[i], 1);
             }
         }
-        
+
         for (Integer key : map.keySet()) {
-            if (map.get(key) > arr.length/3) {
+            if (map.get(key) > arr.length / 3) {
                 System.out.println(key);
-                
+
             }
         }
 
-
-        //4. find out largest subArray with 0  sum
-       int array[] = {15, -2, 2, -8, 1, 7, 10, 23};
+        // 4. find out largest subArray with 0 sum
+        int array[] = { 15, -2, 2, -8, 1, 7, 10, 23 };
         HashMap<Integer, Integer> maps = new HashMap<>();
         int sum = 0;
         int maxLen = 0;
@@ -352,5 +341,30 @@ public static String getStart(HashMap<String,String> tickets){
 
         System.out.println("Length of the largest subarray with sum 0: " + maxLen);
 
+
+
+        // 5.subarray sum equal to k
+        int arrays[] = { 10, 2, -2, -20, 10 };
+        int k = -10;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, 1); // to handle the case when prefix sum itself is k
+
+        int summ = 0;
+        int ans = 0;
+
+        for (int j = 0; j < arrays.length; j++) {
+            summ += arrays[j];
+
+            if (hm.containsKey(summ - k)) {
+                ans += hm.get(summ - k);
+            }
+            int val = 0;
+            if (hm.containsKey(summ)) {
+                val = hm.get(summ);
+            }
+            hm.put(summ, val + 1);
+        }
+
+        System.out.println(ans);
     }
 }
