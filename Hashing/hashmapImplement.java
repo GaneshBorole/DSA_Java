97e516d3d1617e7eb0943cd00dcfe8f1e1c6347a
ganeshborole <ganeshborole73@gmail.com>
2025-07-25 09:44:12 +0530

    union and intersection

--- Hashing/hashmapImplement.java
+++ Hashing/hashmapImplement.java
@@ -249,6 +249,34 @@ public class hashmapImplement {
             }
             System.out.println("ans = "+sets.size());
 
+    
+    // Union & Intersection of 2 arrays
+           
+                int arr1[]={7,3,9};
+                int arr2[]={6,3,9,2,9,4};
+                HashSet<Integer> set1= new HashSet<>();
+                for (int i = 0; i < arr1.length; i++) {
+                    set1.add(arr1[i]); 
+                }
+                for (int i = 0; i < arr2.length; i++) {
+                    set1.add(arr2[i]);
+                }
+                System.out.println("union of array = "+set1.size());
+
+                set1.clear();
+                 for (int i = 0; i < arr1.length; i++) {
+                    set1.add(arr1[i]); 
+                }
+                int count=0;
+                for (int i = 0; i< arr2.length; i++) {
+                    if (set1.contains(arr2[i])) {
+                        count++;
+                        set1.remove(arr2[i]);
+                        
+                    }
+                    
+                }
+                System.out.println("intersection of array ="+count);
 
         //1. find elements that appear more than(n/3) times
         int arr[]={1,3,2,5,1,3,1,5,1};
