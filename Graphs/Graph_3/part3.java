9f5e1a414943785d409c480f54ba4195d7843c2e
ganeshborole <ganeshborole73@gmail.com>
2025-08-02 11:17:40 +0530

    dijkstra algo

--- Graphs/Graph_3/part3.java
+++ Graphs/Graph_3/part3.java
@@ -5,10 +5,12 @@ public class part3 {
     static class Edge {
         int src;
         int dest;
+        int wt;
 
-        public Edge(int s,int d){
+        public Edge(int s,int d,int w){
             this.src=s;
             this.dest=d;
+            this.wt=w;
 
         }   
     }
@@ -16,14 +18,28 @@ static void cerateGraph(ArrayList<Edge> [] graph){
     for (int i=0;i<graph.length;i++){
         graph[i]=new ArrayList<>();
     }
-    graph[2].add(new Edge(2, 3));
-    graph[3].add(new Edge(3, 1));
+    // graph[2].add(new Edge(2, 3));
+    // graph[3].add(new Edge(3, 1));
 
-    graph[4].add(new Edge(4, 1));
-    graph[4].add(new Edge(4, 0));
+    // graph[4].add(new Edge(4, 1));
+    // graph[4].add(new Edge(4, 0));
 
-    graph[5].add(new Edge(5, 0));
-    graph[5].add(new Edge(5, 2));
+    // graph[5].add(new Edge(5, 0));
+    // graph[5].add(new Edge(5, 2));
+
+    graph[0].add(new Edge(0, 1, 2));
+    graph[0].add(new Edge(0, 2, 4));
+
+    graph[1].add(new Edge(1, 3, 7));
+    graph[1].add(new Edge(1, 2, 1));
+
+    graph[2].add(new Edge(2, 4, 3));
+
+    graph[3].add(new Edge(3, 1, 5));
+
+    graph[4].add(new Edge(4, 3, 2));
+    graph[4].add(new Edge(4, 5, 5));
+  
 
 
 }
@@ -76,14 +92,74 @@ public static void printAllpath(ArrayList<Edge> graph[],int src,int dest,String 
         printAllpath(graph, e.dest, dest, path+src);
     }
 }
+
+
+//Dijkstras algorithm
+static class pair implements Comparable<pair>{
+    int n;
+    int path;
+
+    public pair(int n,int path){
+        this.n=n;
+        this.path=path;
+    }
+   @Override
+   public int compareTo(pair p2){
+    return this.path-p2.path;
+   }
+    
+}
+
+public static void  Dijkstras(ArrayList<Edge>[] graph,int src){
+    int dist[]=new int[graph.length];
+    for(int i=0;i<graph.length;i++){
+        if (i !=src) {
+            dist[i]=Integer.MAX_VALUE;//+infinity
+        }
+    }
+    boolean vis []=new boolean[graph.length];  
+    @SuppressWarnings("unchecked")  
+    PriorityQueue<pair> pq= new PriorityQueue<>();
+    pq.add(new pair(src, 0));
+    while (!pq.isEmpty()) {
+        pair curr=pq.remove();
+        if (!vis[curr.n]) {
+            vis[curr.n]=true;
+            for(int i=0;i<graph[curr.n].size();i++){
+                Edge e= graph[curr.n].get(i);
+                int u=e.src;
+                int v=e.dest;
+                int wt=e.wt;
+                if (dist[u]+wt < dist[v]) {
+                    dist[v]=dist[u]+wt;
+                    pq.add(new pair(v, dist[v]));
+                    
+                }
+
+            }
+            
+        }
+        
+    }
+    for(int i=0;i<dist.length;i++){
+        System.out.print(dist[i]+" ");
+    }
+
+System.out.println();
+}
+
     public static void main(String[] args) {
         int V=6;
         @SuppressWarnings("unchecked")
         ArrayList<Edge> graph[]=new ArrayList[V];
         cerateGraph(graph);
        // topsort(graph);
-       int src=5;int dest=1;
-       printAllpath(graph, src, dest, " ");
+    //    int src=5;int dest=1;
+    //    printAllpath(graph, src, dest, " ");
+    int src=0;
+    Dijkstras(graph,src);
+
+
 
         
     }
