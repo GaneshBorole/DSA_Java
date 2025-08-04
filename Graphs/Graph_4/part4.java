fe99e072b12c709733af0397d92e7030c97c26ad
ganeshborole <ganeshborole73@gmail.com>
2025-08-04 10:01:33 +0530

    Prims algo

--- Graphs/Graph_4/part4.java
+++ Graphs/Graph_4/part4.java
@@ -1,8 +1,5 @@
 package Graphs.Graph_4;
-
 import java.util.*;
-
-import Graphs.Graph_4.part4.Edge;
 
 public class part4 {
     static class Edge {
@@ -21,14 +18,31 @@ public class part4 {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0, 2, 4));
+        // graph[0].add(new Edge(0, 1, 2));
+        // graph[0].add(new Edge(0, 2, 4));
 
-        graph[1].add(new Edge(1, 2, -4));
-        graph[2].add(new Edge(2, 3, 2));
+        // graph[1].add(new Edge(1, 2, -4));
+        // graph[2].add(new Edge(2, 3, 2));
 
-        graph[3].add(new Edge(3, 4, 4));
-        graph[4].add(new Edge(4, 1, -1));
+        // graph[3].add(new Edge(3, 4, 4));
+        // graph[4].add(new Edge(4, 1, -1));
+
+
+        //prims
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
+
+        graph[1].add(new Edge(1, 0,10));
+        graph[1].add(new Edge(1, 3, 40));
+
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+
+        graph[3].add(new Edge(3, 0, 30));
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
+
 
     }
 // Bellmanford algorithm (it calculate -ve shortest distance)
@@ -69,12 +83,57 @@ public class part4 {
 
     }
 
+    // Prim's algorithm
+   static class Pair implements Comparable<Pair> {
+        int v;
+        int cost;
+
+        public Pair(int v, int c) {
+            this.v = v;
+            this.cost = c;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.cost - p2.cost;
+        }
+    }
+   public static void Prims(ArrayList<Edge>[] graph) {
+        boolean[] vis = new boolean[graph.length];
+
+        // PriorityQueue of Pair, not Edge
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        pq.add(new Pair(0, 0));
+        int finalCost = 0;
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+
+            if (!vis[curr.v]) {
+                vis[curr.v] = true;
+                finalCost += curr.cost;
+
+                for (int i = 0; i < graph[curr.v].size(); i++) {
+                    Edge e = graph[curr.v].get(i);
+                    if (!vis[e.dest]) {
+                        pq.add(new Pair(e.dest, e.wt));
+                    }
+                }
+            }
+        }
+
+        System.out.println("Final cost of MST = " + finalCost);
+    }
+
     public static void main(String[] args) {
         int V = 6;
         @SuppressWarnings("unchecked")
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
-        BellmanFord(graph, 0);
+       // BellmanFord(graph, 0);
+       Prims(graph);
+
 
     }
 
