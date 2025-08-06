31c4beb7e84e9bd2e5674c20d4d2457962a1a639
ganeshborole <ganeshborole73@gmail.com>
2025-08-06 09:38:07 +0530

    kosaraju algo

---
+++ Graphs/Graph_supplemental/SCC.java
@@ -0,0 +1,96 @@
+package Graphs.Graph_supplemental;
+import java.util.*;
+
+
+public class SCC {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s,int d){
+            this.src=s;
+            this.dest=d;
+
+        }
+    }
+
+    public static void CreateGraph(ArrayList<Edge> [] graph){
+        for (int i = 0; i < graph.length; i++) {
+            graph[i]=new ArrayList<Edge>();  
+        }
+        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1, 0));
+        graph[2].add(new Edge(2, 1));
+        graph[4].add(new Edge(4, 3));
+    }
+
+    //Kosaraju algorithm
+    public static void topsort(ArrayList<Edge> graph[],int curr,boolean vis[],Stack<Integer> s){
+        vis[curr]=true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e= graph[curr].get(i);
+            if (!vis[e.dest]) {
+                topsort(graph, e.dest, vis, s);
+            }
+        }
+        s.push(curr);
+    }
+
+    public static void dfs(ArrayList<Edge> graph[],int curr,boolean vis[]){
+        vis[curr]= true;
+        System.out.print(curr+" ");
+          for(int i=0;i<graph[curr].size();i++){
+            Edge e= graph[curr].get(i);
+            if (!vis[e.dest]) {
+                dfs(graph, e.dest, vis);
+            }
+        }
+    }
+    public static void Kosaraju(ArrayList<Edge> graph[],int V){
+        //step 1
+        Stack<Integer> s= new Stack<>();
+        boolean vis[]=new boolean[V];
+        for(int i=0;i<V;i++){
+            if (!vis[i]) {
+                topsort(graph, i, vis, s);
+                
+            }
+        }
+        //step 2
+        @SuppressWarnings("unchecked")
+         ArrayList<Edge> transpose[]=new ArrayList[V];
+          for (int i = 0; i < graph.length; i++) {
+            vis[i]=false;
+            transpose[i]=new ArrayList<Edge>();  
+        }
+        for (int i = 0; i < V; i++) {
+            for(int j=0;j<graph[i].size();j++){
+                Edge e= graph[i].get(j);
+                transpose[e.dest].add(new Edge(e.dest, e.src));//reverse edge
+
+            }
+            
+        }
+        //step 3
+        while (!s.isEmpty()) {
+            int curr=s.pop();
+            if (!vis[curr]) {
+                System.out.print("SCC -> ");
+                dfs(transpose, curr, vis);
+                System.out.println();
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        int V=5;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[]=new ArrayList[V];
+        CreateGraph(graph);
+        Kosaraju(graph, V);
+        
+    }
+    
+}
