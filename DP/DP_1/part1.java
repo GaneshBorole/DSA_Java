54471ccf4d3c8282074d8d82ff2358c9e04f1ee1
ganeshborole <ganeshborole73@gmail.com>
2025-08-07 10:25:27 +0530

    climbing stairs using tabulation

--- DP/DP_1/part1.java
+++ DP/DP_1/part1.java
@@ -44,6 +44,24 @@ public static int climbingstairs(int n,int ways []){
     ways[n]= climbingstairs(n-1,ways)+climbingstairs(n-2,ways);
     return ways[n];
 }
+
+
+//climbing stairs using tabulation
+public static int climbStairTab(int n){
+    int dp []= new int[n+1];
+    dp[0]=1;
+
+    for(int i=1;i<=n;i++){
+        if(i == 1){
+            dp[i]=dp[i-1]+0;
+        }else{
+        dp[i]=dp[i-1]+dp[i-2];
+        }
+     
+    }
+    return dp[n];
+
+}
     public static void main(String[] args) {
         int n=5;
        // int f[]=new int[n+1];
@@ -52,6 +70,7 @@ public static int climbingstairs(int n,int ways []){
         //System.out.println(fib(n,f));
         System.out.println(fibTabulation(n));
         System.out.println(climbingstairs(n,ways));
+        System.out.println(climbStairTab(n));
         
     }
     
