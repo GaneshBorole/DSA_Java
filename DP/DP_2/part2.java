101ad940dae4b51ecae7dfdef94a1499c5656af4
ganeshborole <ganeshborole73@gmail.com>
2025-08-10 10:04:37 +0530

    knapsack using tabulation

--- DP/DP_2/part2.java
+++ DP/DP_2/part2.java
@@ -19,7 +19,7 @@ public class part2 {
         }
     }
 
-//0-1 knapsack using memoization
+//0-1 knapsack using memoization   T.C(n*w)
  public static int knapsack(int val[],int wt[],int W,int n,int dp[][]){
         if (W==0 || n==0) {
             return 0;
@@ -42,6 +42,46 @@ public class part2 {
         }
     }
 
+    //0-1 knapsack using tabulation
+    public static void print(int dp[][]){
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static int knapsackTab(int val[],int wt[],int W){
+        int n=val.length;
+        int dp[][]=new int[n+1][W+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=0;
+
+        }
+        for(int j=0;j<dp[0].length;j++){
+            dp[0][j]=0;
+
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                int V=val[i-1];
+                int w=wt[i-1];
+                if (w<=j) {//valid
+                    int includeprofit=V+dp[i-1][j-w];
+                    int excludeProfit=dp[i-1][j];
+                    dp[i][j]=Math.max(includeprofit, excludeProfit);   
+                }else{
+                    int excludeProfit=dp[i-1][j];
+                    dp[i][j]= excludeProfit;
+                }
+            }
+        }
+        print(dp);
+        return dp[n][W];
+    }
+
 
 
     public static void main(String[] args) {
@@ -54,7 +94,8 @@ public class part2 {
                 dp[i][j]=-1;
             }
         }
-        System.out.println(knapsack(val, wt, W, val.length,dp));
+        //System.out.println(knapsack(val, wt, W, val.length,dp));
+        System.out.println(knapsackTab(val, wt, W));
         
     }
     
