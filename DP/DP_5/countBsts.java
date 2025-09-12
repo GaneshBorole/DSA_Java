1b10d0845d66305a178fe7387c4b8efeb2019f33
ganeshborole <ganeshborole73@gmail.com>
2025-09-12 10:42:56 +0530

    counts Bsts

---
+++ DP/DP_5/countBsts.java
@@ -0,0 +1,23 @@
+package DP.DP_5;
+import java.util.*;
+
+public class countBsts {
+
+    public static int countBsts(int n){
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2;i< n+1;i++){
+            for(int j=0; j<i ;j++){
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }  
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(countBsts(n));
+    }
+    
+}
