fdc3567a25eb74d755c51940a0156969c0840988
ganeshborole <ganeshborole73@gmail.com>
2025-09-20 17:45:38 +0530

    MCM using Memoization

--- DP/DP_6/MCM.java
+++ DP/DP_6/MCM.java
@@ -1,4 +1,5 @@
 package DP.DP_6;
+import java.util.*;
 
 public class MCM {
     public static int mcmRec(int arr[],int i,int j){
@@ -17,10 +18,37 @@ public class MCM {
         }
         return ans;
     }
+    //MCM using Memoization
+    public static int mcmMemo(int arr[],int i,int j,int dp[][]){
+        if (i==j) {
+            return 0;
+        }
+        if (dp[i][j]!= -1) {
+            return dp[i][j];
+        }
+        int ans=Integer.MAX_VALUE;
+         for (int k = i; k <= j-1; k++) {
+            int cost1=mcmMemo(arr, i, k,dp);// res=arr[i-1].arr[k];
+            int cost2=mcmMemo(arr, k+1, j,dp);// res=arr[k].arr[j];
+            int cost3=arr[i-1]*arr[k]*arr[j];
+            int finaalCost=cost1+cost2+cost3;
+            ans=Math.min(ans, finaalCost);
+
+            
+        }
+        return dp[i][j]=ans;
+
+
+    }
     public static void main(String[] args) {
         int arr[]={1,2,3,4,3};
         int n= arr.length;
-        System.out.println(mcmRec(arr, 1, n-1));
+        //System.out.println(mcmRec(arr, 1, n-1));
+        int dp[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+       System.out.println(mcmMemo(arr, 1, n-1, dp));
         
     }
     
